b63370cc4443499147cd3f26c51b00d328bc989a
Jose Valadez <javaladez90@aol.com>
2023-07-14 23:56:05 -0700

    create a simple counter

---
+++ learning-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HelloWorld from './components/HelloWorld'
+import './App.css';
+import CounterExample from './components/CounterExample';
+
+function App() {
+  return (
+    <div>
+     {/*} <HelloWorld name="Kaitie"/> */}
+     <CounterExample/>
+    </div>
+  );
+}
+
+export default App;
