f4304bb6d058cd3c599ca12ef252d5db27173ee6
Jose Valadez <javaladez90@aol.com>
2023-07-16 00:28:28 -0700

    tailwind css

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import HelloWorld from './components/HelloWorld'
-import './App.css';
-import CounterExample from './components/CounterExample';
+//import HelloWorld from './components/HelloWorld'
+//import CounterExample from './components/CounterExample';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
+//use yarn buildcss in terminal to start css file
+//then yarn start
 function App() {
   return (
     <div>
+      <Header/>
      {/*} <HelloWorld name="Kaitie"/> */}
-     <CounterExample/>
+     <Footer/>
     </div>
   );
 }
---
+++ learning-react/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer className="bg-gray-200 text-center text-xs p-3">
+            &copy; Copyright 2020
+        </footer>
+    )
+}
+
+export default Footer
